a4e99ed54b19da56fccb01ade63163e5d1e7f237
MYCOMPUTER\bluer <a-tamagawa@aqte.net>
2019-03-28 14:42:13 +0900

    乱数の生成と乱数を生成する個数の指定（←途中）

--- Challenges1/js/Challenges1.js
+++ Challenges1/js/Challenges1.js
@@ -1,2 +1,19 @@
 
 'use strict';
+
+
+document.getElementById("button").onclick = function() {
+
+  //入力した数だけランダム生成できるかのテスト
+  var display =document.getElementById("test");
+
+  //0~99の数字をランダムに生成する
+  var rand =Math.floor(Math.random()*100);
+
+  //フォームの値を取得する
+  var input_number =document.getElementById("input_number").value;
+
+
+  display.innerHTML =parseInt(input_number, 10)*(rand);
+
+}
