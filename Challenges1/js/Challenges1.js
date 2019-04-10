9783c62f7c872ebceedf8c7b3c95fda82546d839
MYCOMPUTER\bluer <a-tamagawa@aqte.net>
2019-04-10 17:57:19 +0900

    .filterで表示したい数の範囲の指定（←途中）

--- Challenges1/js/Challenges1.js
+++ Challenges1/js/Challenges1.js
@@ -4,29 +4,63 @@
 
 document.getElementById("button").onclick = function() {
 
-  //入力した数だけランダム生成できるかのテスト
-  var display =document.getElementById("test");
-
-  //0~99の数字をランダムに生成する
-  var rand =Math.floor(Math.random()*100);
-
   //フォームの値を取得する
   var input_number =document.getElementById("input_number").value;
 
   //取得した文字列を数値へ変換する
   var execution_number =parseInt(input_number, 10);
 
+  // 空の配列を作る
   var numbers = [];
 
   var n = 0;
 
+
   while (n < execution_number){
+    //0~99の数字をランダムに生成する
+    var rand =Math.floor(Math.random()*100);
+    // 配列に生成した乱数を新しい要素として追加する
     numbers.push(rand);
     n++;
   }
 
+
+  // 生成した乱数が配列に新しい要素として追加されているかの確認
   console.log(numbers);
 
-  // display.write(numbers);
+  var get_number_1 = numbers.filter(function(element, index, array) {
+    return (element <= 9);
+  })
+  var get_number_2 = numbers.filter(function(element, index, array) {
+    return (element >= 10,<= 19);
+  })
+  var get_number_3 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+  var get_number_4 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+  var get_number_5 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+  var get_number_6 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+  var get_number_7 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+  var get_number_8 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+  var get_number_9 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+  var get_number_10 = numbers.filter(function(element, index, array) {
+    return (element <= );
+  })
+
+
+  document.getElementById("form_0~9").innerHTML = get_number_1;
+  document.getElementById("form_10~19").innerHTML = get_number_2;
 
 };
